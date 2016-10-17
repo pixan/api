050d84996b15b2013acfc449edc2325fd61c2e15
Guillermo Villalobos <guillermo@dvlop.mx>
2016-10-17 17:23:26 -0500

    Request loggins

--- src/middleware/LogRequest.php
+++ src/middleware/LogRequest.php
@@ -10,7 +10,12 @@ class LogRequest
 	public function handle($request, Closure $next)
     {
 		if (config('pixanapi.options.log_requests')) {
-			
+			\DB::table('pixan_api_logs')->insert([
+				'headers' => json_encode($request->header()),
+				'inputs' => json_encode($request->all()),
+				"created_at" =>  \Carbon\Carbon::now(),
+            	"updated_at" => \Carbon\Carbon::now(),
+			]);
 		}
 		return $next($request);
     }
